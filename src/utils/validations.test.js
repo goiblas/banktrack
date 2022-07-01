84696dcfc0b785edcfed38ef11fd95cca64c3ffb
Jesús Olazagoitia Blasco <jesua@cleverppc.com>
2022-07-01 19:40:35 +0200

    test validations

---
+++ src/utils/validations.test.js
@@ -0,0 +1,21 @@
+import { isValidPassword, isValidUsername } from "./validations";
+
+describe('isValidPassword', () => {
+    it('should contain at least one char and one number', () => {
+        const invalidPassword = '12345';
+        expect(isValidPassword(invalidPassword)).toBe(false);
+
+        const validPassword = '123_A';
+        expect(isValidPassword(validPassword)).toBe(true);
+    });
+});
+
+describe('isValidUsername', () => {
+    it('should be a valid email', () => {
+        const invalidUsername = 'jesus';
+        expect(isValidUsername(invalidUsername)).toBe(false);
+
+        const validUsername = 'jesus@olazagoitia.com';
+        expect(isValidUsername(validUsername)).toBe(true);
+    });
+});
